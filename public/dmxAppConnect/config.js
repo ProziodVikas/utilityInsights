39f2b7ab63f15a1b24c77993ded1107d55d487d3
Wappler <git@wappler.io>
2024-04-29 12:34:28 +0530

    sitegroup table sort

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,14 @@
+dmx.config({
+  "siteGroup": {
+    "query": [
+      {
+        "type": "text",
+        "name": "sort"
+      },
+      {
+        "type": "text",
+        "name": "dir"
+      }
+    ]
+  }
+});
